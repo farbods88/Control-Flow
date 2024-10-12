d5040a97b348b64dbd83a94f797d798dde2843ce
Farbod <farbodsaberi@gmail.com>
2024-10-12 12:47:56 +0330

    Update forLoop.cs
    v.1.1

--- forLoop.cs
+++ forLoop.cs
@@ -1,15 +1,16 @@
+
 //for loop 
 
 int maxBullets = 7;
 int currentBullet = 1;
 
-Console.WriteLine ("Reloading Revolver...");
+Console.WriteLine("Reloading Revolver...");
 
 for (int i = currentBullet; i < maxBullets; i++)
 {
- Console.WriteLine ("Currently loaded ammo : " + (i + 1));
- if (i == 6) 
- {
-  Console.WriteLine ("Revolver Fully Reloaded");
- }
+    Console.WriteLine("Currently loaded ammo : " + (i + 1));
+    if (i == 6)
+    {
+        Console.WriteLine("Revolver Fully Reloaded");
+    }
 }
