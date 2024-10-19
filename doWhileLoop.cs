127ab9a0f9e93cf204d123d1566f899f3dd1b0f5
Farbod <farbodsaberi@gmail.com>
2024-10-19 20:23:56 +0330

    Create doWhileLoop.cs
    Here I collected bounties. Sometimes we must repeatedly check if there are more bounties to collect and I simulated that.

---
+++ doWhileLoop.cs
@@ -0,0 +1,11 @@
+
+int bountyCount = 5;
+
+do
+{
+    Console.WriteLine("Arthur collected a bounty. Bounties left : " + bountyCount);
+    bountyCount--;
+}
+while (bountyCount > 0);
+
+Console.WriteLine("All bounties collected.");
